e596a1d0fe5fd9ac3aa745009eff739e956dddde
Fernanda <nanda.kipper@gmail.com>
2024-03-27 17:14:01 -0300

    feat: add auth route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
+import { UserComponent } from './pages/user/user.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,10 @@ export const routes: Routes = [
     {
         path: "signup",
         component: SignUpComponent
+    },
+    {
+        path: "user",
+        component: UserComponent,
+        canActivate: [AuthGuard]
     }
 ];
---
+++ src/app/pages/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+
+}
